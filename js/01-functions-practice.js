cf80546442612ec88c0fe0d29e608fa93f6d04d8
OlenaZh <alyona.mesh@gmail.com>
2021-03-11 21:31:33 -0800

    Step 1.1 added

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -3,11 +3,19 @@
 function halfNumber(number1) {
  var num = number1 / 2;
  return num;
- alert("Half of" + halfNumber + "is" + num);
+ }
+ alert("Half of 5 is " + halfNumber(5));
+
+ //OR STEP 1.1.
+
+ /*var number1 = prompt("Enter number");
+ function halfNumber(number1) {
+    var num = number1 / 2;
+    alert("Half of " + number1 + " is " + num);
 }
-halfNumber(5);
+    halfNumber(number1);*/
 
-
+    
 //STEP 2
 
 //STEP 3

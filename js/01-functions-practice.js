cf12f4ca21e5122bc392ac612dac7442f3a7e041
OlenaZh <alyona.mesh@gmail.com>
2021-03-11 23:39:37 -0800

    step5

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -1,3 +1,5 @@
+
+/*eslint-env browser*/
 //STEP 1
 
 /*function halfNumber(number1) {
@@ -32,10 +34,23 @@ alert("1 is " + percentOf(1, 4) + "% of 4");*/
 
 //STEP 4
 
-function findModulus(num1, num2) {
+/*function findModulus(num1, num2) {
   var result = num2 % num1;
   alert(result + " is the modulus of 4 and 10"); 
 }
-findModulus(4, 10);
+findModulus(4, 10);*/
 
 //STEP 5
+var i, sum;
+var numbers = prompt("Enter numbers delimited by commas");
+var arr = numbers.split(',');
+
+function calculate(numbers) {
+    sum = Number(numbers);
+    for (i=1; i<arguments.length; i+=1) {
+        sum += Number (arguments[i]);
+    }
+    document.write(string(sum) + "<br>");
+}
+
+calculate(numbers);

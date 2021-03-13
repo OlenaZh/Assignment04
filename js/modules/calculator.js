e13e8de30a2e8836d9696207f0cc2b5962d4834f
OlenaZh <alyona.mesh@gmail.com>
2021-03-12 22:34:38 -0800

    export done

--- js/modules/calculator.js
+++ js/modules/calculator.js
@@ -3,11 +3,27 @@
 // ADD FOUR PUBLIC FUNCTIONS BELOW
 
 // ADD FUNCTION
+const add = (x, y) => {
+    return x  +y;
+};
 
 // SUBTRACT FUNCTION
+const sub = (x, y) => {
+    return x - y;
+};
 
 // MULTIPLY FUNCTION
+const multiply = (x, y) => {
+    return x * y;
+};
 
 // DIVIDE FUNCTION
+const divide = (x, y) => {
+    return x / y;
+};
 
 // EXPORT THE FOUR PUBLIC FUNCTIONS
+export {add};
+export {sub};
+export {multiply};
+export {divide};

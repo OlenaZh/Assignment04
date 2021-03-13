48d2a219f6961c9e882b6520949844698e70bea3
OlenaZh <alyona.mesh@gmail.com>
2021-03-12 22:44:52 -0800

    collect numbers

--- js/03-basic-calculator.js
+++ js/03-basic-calculator.js
@@ -2,6 +2,11 @@
 import * as calc from "./modules/calculator.js";
 
 // COLLECT NUMBER 1, NUMBER 2, AND OPERATION FROM THE USER
+var x = prompt("Enter first number");
+ x = Number(numbers);
+ var y = prompt("Enter second number");
+ y = Number(numbers);
+ var operation = prompt("Chose the operation (add, subtract, multiply, divide)");
 
 // CHECK TO SEE WHAT OPERATION THEY'RE USING
 // CALL THE APPROPRIATE FUNCTION
